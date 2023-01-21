051c2ed4e50272a8a6574effa8cd83334416e9ac
nurantezer <nurantezer68@gmail.com>
2023-01-21 14:14:06 +0300

    comments

---
+++ Comments/comments.js
@@ -0,0 +1,26 @@
+//*JavaScript yorumları, JavaScript kodunu açıklamak ve daha okunaklı hale getirmek için kullanılabilir. 
+//*JavaScript yorumları, alternatif kodu test ederken yürütmeyi önlemek için de kullanılabilir.
+
+//* Single Line Comments -Tek Satır Yorumlar
+//* Tek satırlık yorumlar //! ile başlar. // 
+//* ile satırın sonu arasındaki herhangi bir metin, JavaScript tarafından yoksayılır (yürütülmez). 
+//* Bu örnek, her kod satırından önce tek satırlık bir yorum kullanır:
+//* Bu örnek, kodu açıklamak için her satırın sonunda tek satırlık bir açıklama kullanır:
+let x = 9;      // Declare x, give it the value of 5
+let y = x + 4;  // Declare y, give it the value of x + 2
+
+//* Multi-line Comments -Çok Satırlı Yorumlar-
+//* Çok satırlı yorumlar /* ile başlar ve */ ile biter. /* ve */ arasındaki herhangi bir metin JavaScript tarafından yok sayılır. 
+//* Bu örnek, kodu açıklamak için çok satırlı bir yorum (yorum bloğu) kullanır:
+
+/*
+The code below will change
+the heading with id = "myH"
+and the paragraph with id = "myP"
+in my web page:
+*/
+
+//! Tek satırlık açıklamaların kullanılması en yaygın olanıdır. 
+//! Blok yorumlar genellikle resmi belgeler için kullanılır.
+
+//*KOd satırlarının yürütülmesini engellemek için kullanılır. 
