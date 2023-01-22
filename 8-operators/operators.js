26084c3af00ae322447e99d6ae8fd741fe58fe30
nurantezer <nurantezer68@gmail.com>
2023-01-22 12:50:12 +0300

    end

---
+++ 8-operators/operators.js
@@ -0,0 +1,72 @@
+//* JavaScript Operators
+//* Atama Operatörü (=) bir değişkene bir değer atar.
+let x = 10;
+document.getElementById("demo").innerHTML = x;
+//*Types of JavaScript Operators
+//*There are different types of JavaScript operators:
+
+//*Arithmetic Operators
+//*Assignment Operators
+//*Comparison Operators
+//*Logical Operators
+//*Conditional Operators
+//*Type Operators
+
+//*JavaScript Arithmetic Operators
+//* Aritmetik Operatörler, sayılar üzerinde aritmetik işlem yapmak için kullanılır:
+// 
+
+//*JavaScript Assignment Operators
+//*Atama işleçleri, JavaScript değişkenlerine değerler atar. 
+//*Toplama Atama Operatörü (+=), bir değişkene bir değer ekler.
+// let x = 10;
+// x += 5;
+//*	x += y	x = x + y
+
+//*Adding JavaScript Strings
+//*The + operator can also be used to add (concatenate) strings.
+let text1 = "John";
+let text2 = "Doe";
+let text3 = text1 + " " + text2;
+
+//*+= atama operatörü, dizeleri eklemek (birleştirmek) için de kullanılabilir:
+// let text1 = "What a very ";
+// text1 += "nice day";
+// The result of text1 will be:
+
+// What a very nice day
+
+//*Adding Strings and Numbers
+//*İki sayı eklemek toplamı döndürür, ancak bir sayı ve bir dizi eklemek bir dizi döndürür:
+// let x = 5 + 5;
+// let y = "5" + 5;
+// let z = "Hello" + 5;
+// The result of x, y, and z will be:
+
+// 10
+// 55
+// Hello5
+
+//*JavaScript Comparison Operators-JavaScript Karşılaştırma Operatörleri-
+// Operator	Description
+// ==	equal to
+// ===	equal value and equal type
+// !=	not equal
+// !==	not equal value or not equal type
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+// ?	ternary operator
+
+
+//*JavaScript Logical Operators-JavaScript Mantıksal Operatörler-
+// Operator	Description
+// &&	logical and
+// ||	logical or
+// !	logical not
+
+//*JavaScript Type Operators
+// Operator	Description
+// typeof	Returns the type of a variable
+// instanceof	Returns true if an object is an instance of an object type
