4a67dd87cb189928db0ed1ca1546718bab8b43b8
nurantezer <nurantezer68@gmail.com>
2023-01-22 13:27:34 +0300

    end

---
+++ 11-data types/types.js
@@ -0,0 +1,81 @@
+// JavaScript has 8 Datatypes
+// 1. String
+// 2. Number
+// 3. Bigint
+// 4. Boolean
+// 5. Undefined
+// 6. Null
+// 7. Symbol
+// 8. Object
+
+// The Object Datatype
+// The object data type can contain:
+
+// 1. An object
+// 2. An array
+// 3. A date
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+// üstel Gösterim
+// Çok büyük veya çok küçük sayılar bilimsel (üstel) gösterimle yazılabilir:
+// let y = 123e5;       12300000
+// let z = 123e-5;      0.00123
+
+// document.getElementById("demo").innerHTML =
+// y + "<br>" + z;
+
+// JavaScript Dizileri
+// JavaScript dizileri köşeli parantezlerle yazılır.
+
+// Dizi öğeleri virgülle ayrılır.
+
+// Aşağıdaki kod, carsüç öğe (araba adları) içeren adlı bir dizi bildirir (oluşturur):
+
+// Misal
+// const cars = ["Saab", "Volvo", "BMW"];
+// Dizi dizinleri sıfır tabanlıdır; bu, ilk öğenin [0], ikincinin [1] olduğu vb. anlamına gelir.
+
+// Bu öğreticide daha sonra diziler hakkında daha fazla bilgi edineceksiniz .
+
+// JavaScript Nesneleri
+// JavaScript nesneleri kaşlı ayraçlarla yazılır {}.
+
+// Nesne özellikleri, virgülle ayrılmış ad:değer çiftleri olarak yazılır.
+
+// Misal
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// Yukarıdaki örnekteki nesnenin (kişi) 4 özelliği vardır: firstName, lastName, age ve eyeColor.
+
+// Bu öğreticide daha sonra nesneler hakkında daha fazla bilgi edineceksiniz .
+
+// Operatör tipi
+// typeofBir JavaScript değişkeninin türünü bulmak için JavaScript operatörünü kullanabilirsiniz .
+
+// Operatör , typeofbir değişkenin veya ifadenin türünü döndürür:
+
+Misal
+typeof ""             // Returns "string"
+typeof "John"         // Returns "string"
+typeof "John Doe"     // Returns "string"
+
+let car = "";    // The value is "", the typeof is "string"
