9824890f7ee15c1f3416d03d5f62da4faa14f950
nurantezer <nurantezer68@gmail.com>
2023-01-22 13:15:20 +0300

    end

---
+++ 10-assigment/assigment.js
@@ -0,0 +1,69 @@
+//* JavaScript Assignment
+//*+= Operatörü
+// Toplama Atama Operatörü , bir değişkene bir değer ekler.
+
+// Ek Ödev Örnekleri
+// let x = 10;
+// x += 5;
+//*let text = "Hello";
+text += " World";
+document.getElementById("demo").innerHTML = text;
+//*-= Operatör
+// Çıkarma Atama Operatörü , bir değişkenden bir değer çıkarır.
+
+// Çıkarma Atama Örneği
+// let x = 10;
+// x -= 5;
+// *= Operatör
+// Çarpma Atama Operatörü bir değişkeni çarpar.
+
+// Çarpma Atama Örneği
+// let x = 10;
+// x *= 5;
+// **= Operatör
+// Üs Atama Operatörü , bir değişkeni işlenenin gücüne yükseltir .
+
+// Üs Atama Örneği
+// let x = 10;
+// x **= 5;
+// /= Operatör
+// Bölme Atama Operatörü bir değişkeni böler.
+
+// Bölme Atama Örneği
+// let x = 10;
+// x /= 5;
+// %= Operatör
+// Kalan Atama Operatörü , bir değişkene bir kalan atar.
+
+// Kalan Ödev Örneği
+// let x = 10;
+// x %= 5;
+// &&= Operatörü
+// Mantıksal AND atama operatörü iki değer arasında kullanılır .
+
+// İlk değer true ise, ikinci değer atanır.
+
+// Mantıksal AND Atama Örneği
+// let x = 10;
+// x &&= 5;
+// &&=Operatör bir ES2020 özelliğidir .
+
+// ||= Operatörü
+// Mantıksal VEYA atama operatörü iki değer arasında kullanılır .
+
+// İlk değer yanlış ise ikinci değer atanır.
+
+// Mantıksal OR Atama Örneği
+// let x = 10;
+// x ||= 5;
+// ||=Operatör bir ES2020 özelliğidir .
+
+// ??= Operatör
+// Nullish birleştirme atama operatörü , iki değer arasında kullanılır.
+
+// İlk değer undefined veya null ise, ikinci değer atanır.
+
+// Nullish Birleştirme Atama Örneği
+// let x = 10;
+// x ??= 5;
+// ??=Operatör bir ES2020 özelliğidir .
